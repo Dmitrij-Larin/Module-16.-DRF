cec5a8b605cdc7b3df1f460a99a5bad16e4925fb
Dmitrij Larin <larin4921@gmail.com>
2025-05-20 01:17:29 +0300

    Исправлена ошибка в section_serializers.py

--- sections/serializers/section_serializers.py
+++ sections/serializers/section_serializers.py
@@ -2,7 +2,7 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework.fields import SerializerMethodField
 
 from sections.models import Section, SectionContent
-from sections.serializers.sections_content_serializers import SectionContentSectionSerializer
+from sections.serializers.section_content_serializers import SectionContentSectionSerializer
 
 
 class SectionSerializer(ModelSerializer):
